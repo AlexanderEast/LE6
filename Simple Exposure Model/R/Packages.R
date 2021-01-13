598b8534dd7e19ac0390662ef3a10d68faa39609
Alexander East <42595687+AlexanderEast@users.noreply.github.com>
2021-01-13 17:45:20 -0500

    Delete Simple Exposure Model directory
    deleted old upload

--- Simple Exposure Model/R/Packages.R
+++
@@ -1,8 +0,0 @@
-# Required Packages for EPA's Aggregate Exposure Model
-# AE, ORAU, 2020.
-
-requiredpackages<-  c("rio","stringr","plyr","dplyr","readxl")
-neededpackages<-setdiff(requiredpackages,rownames(installed.packages()))
-install.packages(neededpackages)
-suppressWarnings(lapply(requiredpackages, library, character.only = TRUE))
-rm(requiredpackages,neededpackages)
